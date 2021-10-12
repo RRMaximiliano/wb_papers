912fc9204e703319f529cdf3b4f7d18cc0ef26f4
Maximiliano Rodriguez <maximiliano.rod.ra@gmail.com>
2021-10-12 11:30:47 -0600

    :pencil: added dec table

--- 02_plots.R
+++ 02_plots.R
@@ -7,6 +7,8 @@ library(lubridate)
 library(hrbrthemes)
 library(here)
 library(ggstream)
+library(gt)
+library(gtExtras)
 
 # Read data ---------------------------------------------------------------
 
@@ -102,7 +104,7 @@ ggsave(
 tab <- clean_df %>% 
   filter(str_detect(origu, "DIME|DECIE")) %>% 
   arrange(desc(download_count)) %>% 
-  head(10) %>% 
+  head(10) %>%
   mutate(
     teratopic = str_replace_all(teratopic, ",", ", "),
     download_count = scales::comma(download_count)
@@ -214,9 +216,144 @@ tab <- clean_df %>%
   tab_options(
     table.border.top.style = "hidden",
     table.border.bottom.style = "hidden"
+  ) %>% 
+  # Width
+  cols_width(
+    geo_regions ~ px(200)
   )
   
 gtsave_extra(
   data = tab,
   filename = "figs/table_downloads.png"
 )
+
+
+
+# DEC ---------------------------------------------------------------------
+
+tab_dec <- clean_df %>% 
+  filter(str_detect(origu, "DECRG")) %>% 
+  arrange(desc(download_count)) %>% 
+  head(15) %>%
+  mutate(
+    teratopic = str_replace_all(teratopic, ",", ", "),
+    download_count = scales::comma(download_count)
+  ) %>% 
+  select(authors, year, title, teratopic, geo_regions, download_count) %>% 
+  gt() %>% 
+  # gt_hulk_col_numeric(download_count) %>% 
+  gt_merge_stack(col1 = title, col2 = authors) %>% 
+  cols_label(
+    download_count = md("Downloads"),
+    year = "Published",
+    title = md("Title & Authors"),
+    teratopic = md("Topic"),
+    geo_regions = md("Region of Study")
+  ) %>% 
+  tab_header(
+    title = "Most downloaded papers from the Development Research Group Staff at the World Bank",
+    subtitle = "Top 15 papers"
+  )%>%
+  tab_source_note(
+    source_note = md("**Data:** The World Bank Documents & Reports | **Table:** @rrmaximiliano")
+  )%>%
+  tab_footnote(
+    footnote = md("Downloads as of October 7th, 2021"),
+    locations = cells_column_labels(columns = "download_count")
+  ) %>% 
+  # STYLE: Title
+  tab_style(
+    style = list(
+      cell_text(
+        font   = google_font(name = "Roboto Condensed"), 
+        weight = "900",
+        align  = "left", 
+        color  = "#EAAA00"
+      )
+    ),
+    locations = cells_title(groups = "title")
+  ) %>% 
+  # STYLE: Subtitle
+  tab_style(
+    style = list(
+      cell_text(
+        font = google_font(name = "Roboto Condensed"), align = "left"
+      )
+    ),
+    locations = cells_title(groups = "subtitle")
+  ) %>%
+  # STYLE: COLS 
+  tab_style(
+    style = list(
+      cell_text(
+        font  = google_font(name = "Roboto Condensed"), 
+        align = "left", 
+        v_align = "bottom"
+      )
+    ),
+    locations = cells_column_labels(
+      columns = c(authors, year, title, teratopic, geo_regions, download_count)
+    )
+  ) %>% 
+  # STYLE: BODY - NUMBERS
+  tab_style(
+    style = list(
+      cell_text(
+        font = google_font(name = "Inconsolata"),
+        align = "left"
+      )
+    ),
+    locations = cells_body(columns = c(year, download_count))
+  ) %>% 
+  # STYLE: BODY - Others
+  tab_style(
+    style = list(
+      cell_text(
+        font = google_font(name = "Roboto Condensed"),
+        align = "left",
+      )
+    ),
+    locations = cells_body(columns = c(teratopic, geo_regions))
+  ) %>% 
+  tab_style(
+    style = list(
+      cell_text(
+        font = google_font(name = "Roboto Condensed"),
+        align = "left",
+        size = "large"
+      )
+    ),
+    locations = cells_body(columns = c(title, authors))
+  ) %>% 
+  # STYLE: Footnote
+  tab_style(
+    style = list(
+      cell_text(
+        font = google_font(name = "Roboto Condensed"), style = "italic"
+      )
+    ),
+    locations = cells_footnotes()
+  ) %>%
+  tab_style(
+    style = list(
+      cell_text(
+        font = google_font(name = "Roboto Condensed")
+      )
+    ),
+    locations = cells_source_notes()
+  ) %>%
+  # LINES (BORDERS)
+  tab_options(
+    table.border.top.style = "hidden",
+    table.border.bottom.style = "hidden"
+  ) %>% 
+  # Width
+  cols_width(
+    geo_regions ~ px(200)
+  )
+
+gtsave_extra(
+  data = tab_dec,
+  filename = "figs/table_dec_downloads.png"
+)
+
